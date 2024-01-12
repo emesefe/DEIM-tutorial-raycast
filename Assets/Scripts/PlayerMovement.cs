83c0b7ceb55e248daeac5a9a656068e1d3490744
emesefe <maria@frogames.es>
2024-01-12 14:50:53 +0100

    Obstacle Raycast

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,39 +1,20 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D rigidbody2D;
+    private float horizontalInput, forwardInput;
+    private float speed = 8f;
+    private float rotationSpeed = 100f;
 
-    private float horizontalInput;
-    private float moveSpeed = 10f;
-    private float jumpSpeed = 8f;
-
-    private BoxCollider2D boxCollider2D;
-    [SerializeField] private LayerMask groundLayerMask;
-
-    private void Awake()
-    {
-        rigidbody2D = GetComponent<Rigidbody2D>();
-        rigidbody2D.constraints = RigidbodyConstraints2D.FreezeRotation;
-
-        boxCollider2D = GetComponentInChildren<BoxCollider2D>();
-    }
-
-    private void Update()
+    void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rigidbody2D.velocity = Vector2.up * jumpSpeed;
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        rigidbody2D.velocity = new Vector2(moveSpeed * horizontalInput, rigidbody2D.velocity.y);
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime * horizontalInput);
+        
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * forwardInput); 
     }
 }
---
+++ Assets/Scripts/PlayerMovement2D.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2D : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+
+    private float horizontalInput;
+    private float moveSpeed = 10f;
+    private float jumpSpeed = 8f;
+
+    private BoxCollider2D boxCollider2D;
+    [SerializeField] private LayerMask groundLayerMask;
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.constraints = RigidbodyConstraints2D.FreezeRotation;
+
+        boxCollider2D = GetComponentInChildren<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody2D.velocity = Vector2.up * jumpSpeed;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody2D.velocity = new Vector2(moveSpeed * horizontalInput, rigidbody2D.velocity.y);
+    }
+}
