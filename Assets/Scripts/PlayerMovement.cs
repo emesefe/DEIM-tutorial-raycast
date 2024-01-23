dce85b089e5614a4b43ec547f35c0e6d833039e8
emesefe <maria@frogames.es>
2024-01-23 13:24:33 +0100

    Capsule Cast

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,13 +6,22 @@ public class PlayerMovement : MonoBehaviour
     private float speed = 8f;
     private float rotationSpeed = 100f;
 
+    private bool canMove;
+    private float playerRadius = 1f;
+    private float playerHeight = 1.5f;
+
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
         transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime * horizontalInput);
+
+        float moveDistance = speed * Time.deltaTime * forwardInput;
+        canMove = !Physics.CapsuleCast(transform.position, 
+                                       transform.position + Vector3.up * playerHeight,
+                                       playerRadius, transform.forward, moveDistance);
         
-        transform.Translate(Vector3.forward * speed * Time.deltaTime * forwardInput); 
+        if (canMove) { transform.Translate(Vector3.forward * moveDistance); }
     }
 }
